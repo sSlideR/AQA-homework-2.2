5cf077bb8528beb67955f887deea6b2430740fbe
alex <alex92471@gmail.com>
2020-02-16 14:14:34 +0300

    Modified

--- src/test/java/CardOrderServiceTest.java
+++ src/test/java/CardOrderServiceTest.java
@@ -1,14 +1,19 @@
 import com.codeborne.selenide.*;
-import org.junit.jupiter.api.AfterEach;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.CsvFileSource;
+import com.google.gson.internal.bind.util.ISO8601Utils;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.*;
+
+import java.time.LocalDate;
+import java.time.ZoneId;
+import java.util.Calendar;
+import java.util.Date;
 
 import static com.codeborne.selenide.Selenide.*;
 import static com.codeborne.selenide.Condition.*;
 
 public class CardOrderServiceTest {
+
+    String today = LocalDate.now(ZoneId.ofOffset("UTC", "+0300")).toString();
 
     @AfterEach
     void closeBrowser() {
@@ -19,7 +24,7 @@ public class CardOrderServiceTest {
     void ShouldSubmitForm() {
         open("http://localhost:9999");
         SelenideElement form = $("form");
-        form.$("input.input__control[name=name").setValue("Петров Иван");
+        form.$("input[placeholder='Город'").setValue("Саранск");
         form.$("input.input__control[name=phone").setValue("+79210001234");
         form.$("span.checkbox__box").click();
         form.$("button").click();
@@ -27,18 +32,4 @@ public class CardOrderServiceTest {
         $(".Success_successBlock__2L3Cw p").shouldHave(exactText("Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время."));
     }
 
-    @ParameterizedTest
-    @CsvFileSource(resources = "testdata.csv",numLinesToSkip = 1,delimiter = ';',encoding = "Windows-1251")
-    void ShouldAlertWhenInvalidInput(String name, String phone, String message_name, String message_phone) {
-        open("http://localhost:9999");
-        SelenideElement form = $("form");
-        form.$("input.input__control[name=name").setValue(name);
-        form.$("input.input__control[name=phone").setValue(phone);
-
-        form.$("span.checkbox__box").click();
-        form.$("button").click();
-
-        $("[data-test-id=name] span.input__sub").shouldHave(text(message_name));
-        $("[data-test-id=phone] span.input__sub").shouldHave(text(message_phone));
-    }
 }
